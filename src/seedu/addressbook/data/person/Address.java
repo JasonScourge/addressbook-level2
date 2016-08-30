afb3dcff4c9148a3d0271cf54b76cc1a43a27df5
Acer\Jason Scourge <jasonscourge@gmail.com>
2016-08-30 21:41:52 +0800

    Created accessors in case for future use.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -12,6 +12,10 @@ public class Address {
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+"; 
     public static final String COMMA_SPLIT = ", ";
+    public static final int BLOCK_INDEX = 0;
+    public static final int STREET_INDEX = 1;
+    public static final int UNIT_INDEX = 2;
+    public static final int POSTAL_CODE_INDEX = 3;
 
     public final String value;
     private boolean isPrivate;
@@ -32,10 +36,10 @@ public class Address {
         }
         this.value = address;
         String[] temp = address.split(COMMA_SPLIT);
-        this.block = temp[0];
-        this.street = temp[1];
-        this.unit = temp[2];
-        this.postalCode = temp[3];
+        this.block = temp[BLOCK_INDEX];
+        this.street = temp[STREET_INDEX];
+        this.unit = temp[UNIT_INDEX];
+        this.postalCode = temp[POSTAL_CODE_INDEX];
     }
 
     /**
@@ -44,6 +48,24 @@ public class Address {
     public static boolean isValidAddress(String test) {
         return test.matches(ADDRESS_VALIDATION_REGEX);
     }
+    
+    /** Accessors */
+    private String getBlock(){
+    	return block;
+    }
+    
+    private String getStreet(){
+    	return street;
+    }
+    
+    private String getUnit(){
+    	return unit;
+    }
+    
+    private String getPostalCode(){
+    	return postalCode;
+    }
+
 
     @Override
     public String toString() {
