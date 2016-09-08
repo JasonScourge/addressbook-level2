9064660d976ec4e1860789f8dce4131104f11d1f
Acer\Jason Scourge <jasonscourge@gmail.com>
2016-09-08 13:28:26 +0800

    Added nullTests

--- src/seedu/addressbook/common/UtilsTest.java
+++ src/seedu/addressbook/common/UtilsTest.java
@@ -1,5 +1,29 @@
 package seedu.addressbook.common;
 
-public class UtilsTest {
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.*;
 
+public class UtilsTest {
+	@Test
+	public void isAnyNull_noNullArgs() {
+		assertFalse(Utils.isAnyNull(new Object(), new Object(), new Object()));
+	}
+
+	@Test
+	public void isAnyNull_nullArgExists() {
+		assertTrue(Utils.isAnyNull(new Object(), null, new Object()));
+	}
+	
+	@Test
+	public void elementsAreUnique_allUniqueElements() {
+		final List<String> testData = Arrays.asList("Test 1", "Test 2", "Test 3");
+		assertTrue(Utils.elementsAreUnique(testData));
+	}
+	
+	@Test
+	public void elementsAreUnique_notAllUnique() {
+		final List<String> testData = Arrays.asList("Test 1", "Test 1", "Test 3");
+		assertFalse(Utils.elementsAreUnique(testData));
+	}
 }
